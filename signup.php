5e931a152b5e3411ac6ddca0b2c8624b8a71f7f1
zhm426s <zhildenminton1@student.gsu.edu>
2026-04-10 15:49:15 -0400

    Revert "Add form validation"
    This reverts commit 8668c5cb8a7c4e8398c7c77f85c27b432d3226b8.

--- signup.php
+++ signup.php
@@ -1,47 +1,25 @@
 <!DOCTYPE html>
 <html lang="en">
-
-<head>
-    <title>Jeopardy- Signup</title>
-    <meta charset="UTF-8">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<script>
-    function validateForm() {
-        let username = document.forms["signupForm"]["username"].value;
-        let password = document.forms["signupForm"]["password"].value;
-
-        if (username === "" || password === "") {
-            alert("All fields must be filled out");
-            return false;
-        }
-
-        if (password.length < 6) {
-            alert("Password must be at least 6 characters");
-            return false;
-        }
-
-        return true;
-    }
-</script>
-
-<body>
-    <header>
-        <h1>Jeopardy</h1>
-    </header>
-    <main>
-        <h2>Sign Up</h2>
-        <form class="signup-form" method="post" action="login.php" name="signupForm" onsubmit="return validateForm()">
-            <legend>New User</legend>
-            <label for="user">Username: </label>
-            <input type="text" name="user" id="user" placeholder="epictriviafan_123" required>
-            <label for="pass">Password: </label>
-            <input type="password" name="pass" id="pass" required>
-            <button type="submit">Create Account</button>
-        </form>
-        <a href="login.php"><button class="link-button" type="button">Log in with existing account</button></a>
-    </main>
-</body>
-
+    <head>
+        <title>Jeopardy- Signup</title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        <header>
+            <h1>Jeopardy</h1>
+        </header>
+        <main>
+            <h2>Sign Up</h2>
+            <form class="signup-form" method="post" action="login.php">
+                <legend>New User</legend>
+                <label for="user">Username: </label>
+                <input type="text" name="user" id="user" placeholder="epictriviafan_123" required>
+                <label for="pass">Password: </label>
+                <input type="password" name="pass" id="pass" required>
+                <button type="submit">Create Account</button>
+            </form>
+            <a href="login.php"><button class="link-button" type="button">Log in with existing account</button></a>
+        </main>
+    </body>
 </html>
